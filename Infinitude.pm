8c6c601981edfa1a866e84b0895685d5ca7aa824
nebulous <nebulous@crashed.net>
2013-08-15 14:38:55 +0000

    Map moar wunderground Break out config to top Dump weather data

--- Infinitude.pm
+++ Infinitude.pm
@@ -1,16 +1,36 @@
 #!/usr/bin/env perl
 
 package Infinitude;
-use Web::Simple 'Infinitude';
+use Web::Simple __PACKAGE__;
 use Plack::Middleware::TemplateToolkit;
-
-use URI::Encode qw/uri_encode uri_decode/;
+use WWW::Wunderground::API;
+use Cache::FileCache;
 use XML::Simple;
 use DateTime;
-
 use Data::Dumper;
+
+sub default_config {
+  tt=>{
+    zip=>'22152',
+    time_zone=>'America/New_York',
+    forecast_ping=>240,
+    status_ping=>60
+  },
+  wunderground=>{
+      location=>'22152', 
+      api_key=>'your wunderground key', 
+      auto_api=>1,
+      cache=>Cache::FileCache->new({ namespace=>'infinitude', default_expires_in=>60*240 }) #Any cache should do.
+  }
+}
+
 sub dispatch_request {
   my ($self, $env) = @_;
+
+  my $wunderground = new WWW::Wunderground::API($self->config->{wunderground});
+
+  #Mangle Internet-bound request to self
+  #Plack::App::Proxy and friends may do this better
   if ($env->{HTTP_HOST} !~ /$env->{SERVER_NAME}/) {
     $env->{HTTP_HOST} = $env->{SERVER_NAME}.':'.$env->{SERVER_PORT};
     $env->{PATH_INFO} =~ s|http://[^/]+||;
@@ -24,33 +44,29 @@ sub dispatch_request {
   },
   #GET http://128.11.138.31/weather/22152/forecast
   sub (GET + /weather/:zip/forecast) {
-    $env->{'tt.vars'}{dt} = DateTime->now(time_zone=>'GMT');
     $env->{'tt.template'} = 'forecast.xml';
+    $env->{'tt.vars'}{dt} = DateTime->now(time_zone=>'GMT');
+    $env->{'tt.vars'}{wunderground} = $wunderground;
+    open(F,">root/weather.txt"); print F Dumper($wunderground->data); close(F);
     return;
   },
-  #POST http://128.11.138.31/systems/2512W003720
+  #POST http://128.11.138.31/systems/2500W003210
   sub (POST + /systems/:system_id + %data=) {
     my ($self, $system_id, $data) = @_;
     open(F,">root/systems.txt"); print F Dumper(XMLin($data)); close(F);
     [ 200, [], [] ]
   },
-  #POST http://128.11.138.31/systems/2512W003720/status
+  #POST http://128.11.138.31/systems/2500W003210/status
   sub (POST + /systems/:system_id/status + %data=) {
     my ($self, $system_id, $data) = @_;
-    print STDERR "---------------- status ------------------\n";
     open(F,">root/status.txt"); print F Dumper(XMLin($data)); close(F);
     $env->{'tt.vars'}{ping_rate} = 120; 
     $env->{'tt.template'} = 'status.xml';
     return;
   },
-  #POST http://128.11.138.31/systems/2512W003720/notifications
-  sub (/systems/:system_id/notifications) {
+  #POST http://128.11.138.31/systems/2500W003210/notifications
+  sub (POST + /systems/:system_id/notifications) {
     [ 200, [], [] ]
-  },
-  sub (/test) {
-    $env->{'tt.template'} = 'forecast.xml';
-    $env->{'tt.vars'}{dt} = DateTime->now(time_zone=>'GMT');
-    return;
   },
   sub (GET + /) {
     $env->{'tt.template'} = 'index.html';
@@ -59,9 +75,8 @@ sub dispatch_request {
   sub () {
     my $self = shift;
     my $env = shift;
-    print STDERR "Fallback called\n";
     return $env->{'tt.template'} 
-      ? Plack::Middleware::TemplateToolkit->new(INCLUDE_PATH=>'root')
+      ? Plack::Middleware::TemplateToolkit->new(INCLUDE_PATH=>'root', vars=>$self->config->{tt})
       : [ 405, [ 'Content-type', 'text/plain' ], [ 'Method not allowed' ] ];
   }
 }
