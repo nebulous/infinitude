eba3ba9738a8ace9a375144ce6ae3d8d8f5ddbd5
nebulous <nebulous@crashed.net>
2013-09-29 22:54:29 +0000

    Remove broken startup. Add proper startup. Clean up parser.

--- sniff/parse_rs485.pl
+++ sniff/parse_rs485.pl
@@ -4,9 +4,20 @@ use Moo;
 has 'name' => (is=>'rw');
 has 'type' => (is=>'rw');
 has 'index' => (is=>'rw');
+has 'attrs' => (is=>'rw', lazy=>1, default=>sub{{}});
+
+sub BUILDARGS {
+  my ( $class, @args ) = @_;
+  if (@args % 2 == 1) {
+    my ($type,$index) = split(/\./, shift(@args));
+    return { type=>$type, index=>$index };
+  }
+  return @args ;
+}
 
 sub BUILD {
   my $self = shift;
+
   my $msb_h = sprintf("%02x",$self->type);
   $self->name(sprintf("Device: %02x%02x", $self->type, $self->index));
   $self->name("Thermostat".$self->index) if $msb_h =~ /^2/;
@@ -16,7 +27,13 @@ sub BUILD {
 
 sub address {
   my $self = shift;
-  return chr($self->type).chr($self->index);
+  my ($add) = @_;
+  if ($add) {
+    my ($type,$index) = split(/\./,$add);
+    $self->type($type);
+    $self->index($index);
+  }
+  return $self->type.'.'.$self->index;
 }
 
 
@@ -39,8 +56,12 @@ sub BUILD {
   my $self = shift;
   my ($dst_type, $dst_index, $src_type, $src_index, $length, $options, $function) = unpack("CCCCCnC", $self->raw);
   #use Data::Dumper; print Dumper([$dst_type, $dst_index, $src_type, $src_index, $length, $reserved, $function]);
-  $self->dst(new CarrierDevice({type=>$dst_type, index=>$dst_index}));
-  $self->src(new CarrierDevice({type=>$src_type, index=>$src_index}));
+
+  $self->dst("$dst_type.$dst_index");
+  $self->src("$src_type.$src_index");
+  #$self->dst(new CarrierDevice({type=>$dst_type, index=>$dst_index}));
+  #$self->src(new CarrierDevice({type=>$src_type, index=>$src_index}));
+
   $self->options($options);
   $self->length($length);
   $self->function($function);
@@ -48,7 +69,6 @@ sub BUILD {
 
 package CarrierFrame;
 use Moo;
-use Digest::CRC qw/crc16/;
 
 has 'raw'=>(is=>'ro');
 has 'header'=>(is=>'rw');
@@ -69,22 +89,77 @@ sub BUILD {
   $self->checksum(unpack("n",substr($self->raw,-2)));
 }
 
-#class method to pull the next valid frame from a string
-#does not appear to recognize broadcast frames.
+sub type {
+  my $self = shift;
+  my $types = {
+    6 => 'reply',
+    11 => 'request',
+  };
+  return $types->{$self->header->function} || 'type-'.$self->header->function;
+}
+
+sub register {
+  my $self = shift;
+  join(':',unpack("ccc",substr($self->data,0,3)));
+}
+
+
+package CarrierStream;
+use Moo;
+use Digest::CRC qw/crc16/;
+
+has data=>(is=>'rw', default=>sub{''});
+has devices=>(is=>'rw', default=>sub{{}});
+
+sub add {
+  my $self = shift;
+  my ($data) = @_;
+  $data||='';
+  $self->data($self->data.$data);
+}
+
+sub remove {
+  my $self = shift;
+  my ($start,$length) = @_;
+  unless ($length) {
+    $length=$start;
+    $start = 0;
+  }
+  my $sl =length($self->data);
+  my $dc = $self->data;
+  substr($dc,$start,$length,'');
+  $self->data($dc);
+}
+
+#class method to pull the next valid frame from stream
+#does not appear to recognize broadcast frames. No crc?
+my $frame_log = {};
 sub get_frame {
-  my $class = shift;
-  my ($input) = @_;
-  $input = $class unless $class eq 'CarrierFrame';
+  my $self = shift;
   my $offset=0;
-  while ($offset<length($$input)) {
-    return 0 if length($$input)<($offset+4);
-    my $len = 10+ord(substr($$input,$offset+4,1));
-    return 0 if length($$input)<=(10+$len);
-    my $tf = substr($$input,$offset, 10+ord(substr($$input,$offset+4,1)));
+  my $data_len = length($self->data());
+
+  while ($offset<$data_len) {
+    return 0 if $data_len<($offset+4);
+    my $len = 10+ord(substr($self->data,$offset+4,1));
+    return 0 if $data_len<$len;
+    my $tf = substr($self->data,$offset, $len);
     if (crc16($tf) == 0) {
-      print "Found frame at offset $offset\n";
-      substr($$input,0,$offset+$len,'');
-      return new CarrierFrame($tf);
+      print "Found frame at offset $offset\n" if $offset;
+      $self->remove($offset+$len);
+      $frame_log->{$tf}||=0;
+      $frame_log->{$tf}++;
+      my $frame = new CarrierFrame($tf);
+
+      if ($frame->type eq 'reply') {
+        my $device_addr = $frame->header->src;
+        my $device = $self->devices->{$device_addr} ||= new CarrierDevice($device_addr);
+        $device->attrs->{$frame->register}||={};
+        $device->attrs->{$frame->register}{$frame->data}||=0;
+        $device->attrs->{$frame->register}{$frame->data}++;
+      }
+
+      return $frame;
     }
     $offset++;
   }
@@ -92,10 +167,10 @@ sub get_frame {
   return 0;
 }
 
+sub gl { return $frame_log };
+
 
 1;
-
-
 
 sub hexdump {
   my $ret ='';
@@ -117,20 +192,46 @@ sub asciidump {
 
 no warnings 'uninitialized';
 
+use Data::Dumper;
+
 my $frame_count=0;
-my $tries=0;
-my $input = '';
+my $stream = new CarrierStream;
 while (<>) {
-  $input.=$_;
-  my $frame = CarrierFrame->get_frame(\$input);
-  print "Tries: $tries frames: $frame_count length".length($input)."\n";
-  $tries++;
-  next if $frame == 0;
-  print "--------------------- Frame $frame_count -------------------------\n";
-  $frame_count++;
-  print join(" ", "Message type", $frame->header->function == 11 ? 'request' : $frame->header->function == 6 ? 'reply' : $frame->header->function,"from", $frame->header->src->name, "to", $frame->header->dst->name)."\n";
-  print &hexdump($frame->data)."\n";
-  print &asciidump($frame->data)."\n";
-  #exit if $frame_count>3;
+  $stream->add($_);
+  while(my $frame = $stream->get_frame) {
+    print "Frames: $frame_count buffer length".length($stream->data)."\n";
+    print "--------------------- Frame $frame_count -------------------------\n";
+    print join(" ", "Message type", $frame->type,"from", $frame->header->src, "to", $frame->header->dst)."\n";
+    print &hexdump($frame->data)."\n";
+    print &asciidump($frame->data)."\n";
+    $frame_count++;
+  }
+  last if $frame_count>=50;
 }
+
+#Search for changing data in stream
+=comment
+foreach my $dev_id (keys %{$stream->devices}) {
+  my $dev = $stream->devices->{$dev_id};
+  print "\n".$dev->name."\n";
+  foreach my $attr (keys %{$dev->attrs}) {
+    print "\t$attr\n";
+    foreach my $val (keys %{$dev->attrs->{$attr}}) {
+      my $count = $dev->attrs->{$attr}{$val};
+      print "\t(x$count)\n";
+      print "\t\t".&hexdump($val)."\n";
+      print "\t\t".&asciidump($val)."\n";
+    }
+  }
+}
+=end
+
+=comment
+my $log = $stream->gl;
+foreach my $fr (sort{$log->{$b} <=> $log->{$a}} keys %$log) {
+  print "Count ".$log->{$fr}."\n";
+  print &hexdump($fr)."\n";
+  print &asciidump($fr)."\n\n";
+}
+=cut
 
