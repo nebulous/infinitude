b604ab80e77eddf5671c1aff8c0cec75c5b371c5
John Lifsey <john.lifsey@inspire.com>
2018-12-18 09:20:31 -0500

    fallback to XML-based outside temp

--- public/app/scripts/controllers/serial.js
+++ public/app/scripts/controllers/serial.js
@@ -1,7 +1,6 @@
 'use strict';
 function wsu(s) {
 	var l = window.location;
-	//return 'ws://10.0.8.31:80'+s;
 	return ((l.protocol === 'https:') ? 'wss://' : 'ws://') + l.hostname + (((l.port !== 80) && (l.port !== 443)) ? ':' + l.port : '') + s;
 }
 
