7ccfb9f65975823b8df6d4e891e94ed50c4d9b57
Michael Marley <michael@michaelmarley.com>
2022-03-05 08:20:02 -0500

    Don't update profile/schedule data while user is editing
    This fixes the issue where a timed (or manual) data update from
    the server would clobber any changes the user had made to the
    profile or schedule pages, which is very annoying while attempting
    to set up a schedule or make multiple changes to the profiles.  It
    works by keeping a copy of the system data that is used for the
    profile and schedule pages.  If the user has not edited anything,
    the copy is refreshed on every update to match the existing
    behavior.  If the user does make a change, the updates to the copy
    stop until the user either saves the changes or reverts them.  The
    color of the globe icon is changed to magenta to indicate this
    condition.  Clicking the globe icon to do a refresh while there are
    pending changes displays a confirmation message to indicate that
    this action will overwrite the changes.  The updating of the status
    and other data continues unchanged even when edits to the system
    data have been made.  Additionally, the debounce logic has been
    removed and the Save buttons have been reconfigured to only allow
    saving when there is actually data to save.

--- public/app/scripts/controllers/main.js
+++ public/app/scripts/controllers/main.js
@@ -1,10 +1,20 @@
 'use strict';
 angular.module('infinitude')
   .controller('MainCtrl', function ($scope, $http, $interval, $timeout, $location) {
-		$scope.debounce = 0;
+		const GLOBE_COLOR_LOADING='#16F';
+		const GLOBE_COLOR_CONNECTED='#44E';
+		const GLOBE_COLOR_UNSAVED_CHANGES='#F0F';
+		const GLOBE_COLOR_ERROR='#E44';
+
 		$scope.carbus = $scope.carbus||{};
 
 		$scope.selectedZone = 0;
+
+		$scope.systemsEdit = null;
+		// Null indicates the "systems" data has never been copied to "systemsEdit"
+		// False means the data has been copied and is currently the same as "systems"
+		// True means the data has been copied and the user has edited it
+		$scope.systemsEdited = null;
 
 		$scope.empty = function(input) { console.log(input); };
 		$scope.mkTime = function(input) {
@@ -33,50 +43,82 @@ angular.module('infinitude')
 		}
 
 		var globeTimer;
-		$scope.reloadData = function() {
-			if ($scope.debounce > 0) {
-				$scope.debounce = $scope.debounce - 1;
+		$scope.reloadData = function(userInitiated) {
+			if (userInitiated && $scope.systemsEdited) {
+				if (confirm('This will erase your unsaved changes')) {
+					$scope.systemsEdited = null;
+				}
 			}
 			var keys = ['systems','status','notifications','energy'];
 			angular.forEach(keys, function(key) {
-				$scope.globeColor = '#16F';
+				$scope.globeColor = GLOBE_COLOR_LOADING;
 				$http.get('/'+key+'.json')
 					.then(function(response) {
 						var rkey = key;
-						if (rkey === 'systems') { rkey = 'system'; }
+						if (rkey === 'systems') {
+							rkey = 'system';
+							// If "systemsEdit" hasn't been populated or hasn't been edited, deep-copy
+							// "systems" to it to provide a copy for the user to edit without having
+							// the automatic refresh wipe the edits
+							if ($scope.systemsEdited === false || $scope.systemsEdited === null) {
+								$scope.systemsEdit = angular.copy(response.data[rkey][0]);
+							}
+						}
 						//console.log(key,rkey,response.data);
 						$scope[key] = store[key] = response.data[rkey][0];
-						$scope.globeColor = '#44E';
+						// If "systemsEdit" has been edited, don't switch away from the magenta globe
+						if ($scope.systemsEdited !== true) {
+							$scope.globeColor = GLOBE_COLOR_CONNECTED;
+						}
 						$timeout.cancel(globeTimer);
-						globeTimer = $timeout(function() { $scope.globeColor = '#E44'; }, 4*60*1000);
+						globeTimer = $timeout(function() {
+							$scope.globeColor = GLOBE_COLOR_ERROR;
+						}, 4*60*1000);
 					},
 					function() {
-						$scope.globeColor = '#E44';
+						$scope.globeColor = GLOBE_COLOR_ERROR;
 						console.log('oh noes!',arguments);
 					});
 			});
 			window.localStorage.setItem('infinitude', angular.toJson(store));
 		};
 
-		$scope.$watch('systems', function(newValue,oldValue) {
-			if (newValue!==oldValue) {
-				$scope.debounce = $scope.debounce + 1;
+		$scope.$watch('systemsEdit', function() {
+			if ($scope.systemsEdit !== null) {
+				// If "systemsEdit" was not populated previously or the user undid all changes
+				// (making "systemsEdit" equal to "systems" once again), mark the data as not
+				// having been edited
+				// If "systemsEdit" was already populated, this indicates a user edit
+				if ($scope.systemsEdited === null || angular.equals($scope.systems, $scope.systemsEdit)) {
+					$scope.systemsEdited = false;
+					$scope.globeColor = GLOBE_COLOR_CONNECTED;
+				} else if ($scope.systemsEdited === false) {
+					$scope.systemsEdited = true;
+					$scope.globeColor = GLOBE_COLOR_UNSAVED_CHANGES;
+				}
 			}
 		}, true);
 
-		$scope.reloadData();
-		$interval($scope.reloadData,3*60*1000);
+		$scope.reloadData(false);
+		$interval($scope.reloadData, 3*60*1000, 0, true, false);
 
 		$scope.isActive = function(route) {
 			return route === $location.path();
 		};
+
 		$scope.save = function() {
+			// Deep-copy "systemsEdit" back to "systems" so that any more
+			// changes made before the next reload will appear correctly
+			$scope.systems = angular.copy($scope.systemsEdit);
 			var systems = { 'system':[$scope.systems] };
 			//console.log('saving systems structure', systems);
 			$http.post('/systems/infinitude', systems )
 				.then(function() {
-					$scope.debounce = 0;
-					setTimeout(function() { if ($scope.debounce === 0) { $scope.reloadData(); }}, 10*1000);
+					setTimeout(function() {
+						$scope.reloadData(false);
+					}, 10*1000);
+					$scope.systemsEdited = false;
+					$scope.globeColor = GLOBE_COLOR_CONNECTED;
 				},
 				function() {
 					console.log('oh noes! save fail.');
@@ -86,4 +128,4 @@ angular.module('infinitude')
 		$scope.selectZone = function(zone) {
 			$scope.selectedZone = zone;
 		};
-  });
+	});
