6326a8e0bff401aad1da701675a1050ddc398603
davidtaillefer <57071802+davidtaillefer@users.noreply.github.com>
2026-08-04 17:51:49 -0400

    Made Temperature Step conditional on C or F

--- lib/Infinitude/MQTT.pm
+++ lib/Infinitude/MQTT.pm
@@ -121,7 +121,7 @@ sub publish_discovery {
                 preset_mode_command_topic    => "$zbase/preset/cmd",
                 action_topic                 => "$zbase/action",
                 optimistic                   => 1,
-                temp_step                    => 0.5,
+                temp_step                    => $cfgem =~ /c/i ? 1 : 0.5,
                 min_temp                     => $cfgem =~ /c/i ? 4 : 40,
                 max_temp                     => $cfgem =~ /c/i ? 37 : 99,
                 temperature_unit             => $cfgem =~ /c/i ? 'C' : 'F',
